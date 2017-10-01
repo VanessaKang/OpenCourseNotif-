ca754cdd8d1c1f86dc7fb897807a1853295d9c83
Vanessa Kang <kangv2@mcmaster.ca>
2017-10-01 01:51:56 -0400

    Setting up the header of the file

---
+++ opencoursenotif.py
@@ -0,0 +1,7 @@
+#############################
+#Name: Vanessa Kang
+#Date started: September 30, 2017
+#Purpose: Determine when closed course is open so that notified students can enroll
+#Version: 1
+#############################
+
